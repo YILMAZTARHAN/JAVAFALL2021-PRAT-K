bdf23ee1557597d163d17a7d88e06013bb747c48
Yılmaz Tarhan <aslankral3325@gmail.com>
2021-12-08 00:29:54 +0300

    10.gün

---
+++ src/Day10_14_Encapsulation/Q01/ArabaMain.java
@@ -0,0 +1,21 @@
+package Day10_14_Encapsulation.Q01;
+
+public class ArabaMain {
+
+	public static void main(String[] args) {
+		Araba honda=new Araba();// paramtresiz cons honda obj create edildi
+		//honda.setMotor(1300);
+		//honda.setModel("civic");
+		//honda.setYil(20212);
+		//honda.getModel();
+		
+		//System.out.println(honda.getYil());
+		
+		Araba volvo=new Araba("xc9o","beyaz",20,2028);
+		Araba ww=new Araba("passat","kýrmýzý",1600,-2021);
+		System.out.println("sectiginiz arac volvo -> "+"yili : "+volvo.getYil()+" renk : "+volvo.getRenk()+" motor : "+volvo.getMotor());
+	    System.out.println("sectiginiz arac ww -> "+"yili : "+ww.getYil()+" renk : "+ww.getRenk()+" motor : "+ww.getMotor());
+
+	}
+
+}
